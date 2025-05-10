31c3a3d55b2fe05714da4a75d6e038ea06e05ca6
Felipe Asenjo <59419221+FelipeAsenjo@users.noreply.github.com>
2025-05-10 19:17:51 -0400

    Overlay round buttons created

---
+++ src/components/RoundedButton.vue
@@ -0,0 +1,27 @@
+<script setup>
+
+</script>
+
+<template>
+    <button class="rounded-button">
+        <slot />
+    </button>
+</template>
+
+<style lang="scss" scoped>
+.rounded-button {
+    background-color: $color-gray-300;
+    width: 52px;
+    height: 52px;
+    border-radius: $radius-round;
+    border: none;
+    margin-right: $spacing-sm;
+    cursor: pointer;
+    transition: transform 0.2s ease;
+
+
+    &:hover {
+        transform: scale(1.05);
+    }
+}
+</style>
--- src/components/VideoControlOverlay.vue
+++ src/components/VideoControlOverlay.vue
@@ -1,13 +1,86 @@
+<script setup>
+import RoundedButton from '@/components/RoundedButton.vue'
+
+const iconSize = 20
+
+const handleTopMenuClick = () => {
+  console.log('handleTopMenuClick')
+}
+
+const handlePlayClick = () => {
+  console.log('handlePlayClick')
+}
+
+const handleVolumeClick = () => {
+  console.log('handleVolumeClick')
+}
+
+const handleDetailsClick = () => {
+  console.log('handleDetailsClick')
+}
+</script>
+
 <template>
-  <div class="video-control">
-    video control
+  <div class="video_control">
+    <section class="top">
+      <div class="top-left">
+        <RoundedButton @click="handleTopMenuClick">
+          <img src="../assets/svg/top-menu_icon.svg" :width="iconSize" :height="iconSize"/>
+        </RoundedButton> 
+      </div>
+      <div class="top-right">
+
+      </div>
+    </section>
+    <section class="bottom">
+      <div class="media">
+        <div class="left">
+          <RoundedButton @click="handlePlayClick">
+            <img src="../assets/svg/play_icon.svg" :width="iconSize" :height="iconSize"/>
+          </RoundedButton> 
+          <RoundedButton @click="handleVolumeClick">
+            <img src="../assets/svg/volume_icon.svg" :width="iconSize" :height="iconSize"/>
+          </RoundedButton> 
+        </div>
+        <div class="right">
+          <RoundedButton class="no-margin" @click="handleDetailsClick">
+            <img src="../assets/svg/details_icon.svg" :width="iconSize" :height="iconSize"/>
+          </RoundedButton> 
+        </div>
+      </div>
+      <div class="hotkeys">
+        <div class="left">
+
+        </div>
+        <div class="right">
+
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
 <style lang="scss" scoped>
-.video-control {
+.video_control {
   padding: $spacing-xl $spacing-xl $spacing-md $spacing-xl;
   height: 100%;
   width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+
+  .top, .bottom .media {
+    height: fit-content;
+    width: 100%;
+  }
+
+  .bottom .media {
+    display: flex;
+    justify-content: space-between;
+  }
+
+  .no-margin {
+    margin: 0;
+  }
 }
 </style>
