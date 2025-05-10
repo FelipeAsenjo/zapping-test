255eb0f680d29db522c6d6451e54bc32cf52b89f
Felipe Asenjo <59419221+FelipeAsenjo@users.noreply.github.com>
2025-05-10 04:36:13 -0400

    Add background image to the player

--- src/views/MoviePlayer.vue
+++ src/views/MoviePlayer.vue
@@ -1,9 +1,21 @@
 <template>
-  <main class="movie-player">
-    <h1>Movie Player</h1>
+  <main class="movie_player--page">
+    <div class="movie_player--player">
+
+      <img src="../assets/images/playing-movie_bg_image.webp" />
+    </div>
   </main>
 </template>
 
 <style lang="scss" scoped>
+.movie_player--page {
+  display: grid;
+  place-items: center;
+  height: 100vh;
+  background-color: $color-black;
+}
 
+.movie_player--player {
+  height: 80%;
+}
 </style>
