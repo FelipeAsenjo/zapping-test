a465b0943e75e244068dbeeb79ae0f69860a7732
Felipe Asenjo <59419221+FelipeAsenjo@users.noreply.github.com>
2025-05-10 07:35:39 -0400

    Create const file

---
+++ CONST.js
@@ -0,0 +1,10 @@
+export const envs = {
+    DEV: 'development',
+    PROD: 'production'
+}
+
+export const model = {
+    AVAILABLE_LANGUAGES: 'availableLanguages',
+    AVAILABLE_CHANNELS: 'availableChannels',
+    TV_SHOW_DETAILS: 'tvShowDetails',
+}
