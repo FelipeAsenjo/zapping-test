ce90ea5523652e8582308bb620b455862db6846e
Felipe Asenjo <59419221+FelipeAsenjo@users.noreply.github.com>
2025-05-12 02:46:25 -0400

    Clean up

--- src/components/OverlayHorizontal.vue
+++ src/components/OverlayHorizontal.vue
@@ -9,9 +9,6 @@
   position: fixed;
   inset: 0;
   background: linear-gradient(90deg, black, transparent, black);
-  // display: flex;
-  // align-items: center;
-  // justify-content: center;
   z-index: 1200;
 }
 </style>
